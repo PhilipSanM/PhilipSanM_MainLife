e524271f71cfedc0d93afe53169e6bbd724b8d4a
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-11-25 20:05:46 -0600

    Updated resume and services

--- phlpsnm/src/constants/services.js
+++ phlpsnm/src/constants/services.js
@@ -3,6 +3,7 @@ import React from "react"
 
 import { SiIbm } from "@react-icons/all-files/Si/SiIbm"
 import { FaMicrosoft } from "@react-icons/all-files/fa/FaMicrosoft"
+import { FaCode } from "@react-icons/all-files/fa/FaCode"
 
 const services = [
   {
@@ -16,6 +17,12 @@ const services = [
     icon: <FaMicrosoft className="service-icon" />,
     title: "MLSA",
     text: "Microsoft Learn Student Ambassador aimed at students who are passionate about technology and want to share their knowledge with others. It offers opportunities to learn, grow, and connect with like-minded individuals in the technology industry.",
+  },
+  {
+    id: 3,
+    icon: <FaCode className="service-icon" />,
+    title: "ESCOM ACM Student Chapter",
+    text: "Participated in the ACM ICPC 2021-2023 programming contests as a member of my university's algorithms club.",
   },
 ]
 
